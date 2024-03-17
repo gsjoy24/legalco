ce8b8bd2adb9c5c89cf5bf0c2f007aa64e5b2c3c
gsjoy24 <gour.joy24@gmail.com>
2024-03-17 17:44:00 +0600

    done

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		ViteImageOptimizer({
-			jpg: {
-				quality: 80
-			},
-			jpeg: {
-				quality: 80
-			},
-			png: {
-				quality: 80
-			},
-			webp: {
-				quality: 80
-			}
-		})
-	]
+	plugins: [react()]
 });
